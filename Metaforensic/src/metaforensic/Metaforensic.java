3d7b193e2bf07be7cfc1fead7b5839928e839526
andres hernandez <andy1818ster@gmail.com>
2013-03-22 13:39:12 -0600

    Inicio
    Inicio de proyecto

---
+++ Metaforensic/src/metaforensic/Metaforensic.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metaforensic;
+
+/**
+ *
+ * @author andy737-1
+ */
+public class Metaforensic {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
